608df26a6951fb4ace776e1381b45b6386c48a49
fmaticSRB <51976766+fmaticSRB@users.noreply.github.com>
2019-08-06 14:33:49 -0700

    Updated formatting

--- Week2 - Deques_And_Randomized_Queues/src/Permutation.java
+++ Week2 - Deques_And_Randomized_Queues/src/Permutation.java
@@ -1,14 +1,24 @@
+ /***********************************************************
+ * AUTHOR: Filip Matic
+ * Title: Permutation.java
+ * Description: Takes an integer, k, as a command line
+ * argument, reads a sequence of strings from standard input
+ * and prints exactly k of them, uniformly at random.
+ ************************************************************/
 import edu.princeton.cs.algs4.StdIn;
 
 import java.util.Iterator;
 
-public class Permutation {
-    public static void main(String[] args) {
+public class Permutation 
+{
+    public static void main(String[] args) 
+    {
         RandomizedQueue<String> rque = new RandomizedQueue<>();
         int num = Integer.parseInt(args[0]);
-        //In in = new In(args[1]);
+
         String str;
-        while (!StdIn.isEmpty()) {
+        while (!StdIn.isEmpty()) 
+        {
             str = StdIn.readString();
             rque.enqueue(str);
         }
