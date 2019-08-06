0d0ee9083f397b6b7a4f774168a07d1d4a862429
fmaticSRB <51976766+fmaticSRB@users.noreply.github.com>
2019-08-06 16:11:56 -0700

    Fixed API formatting issue

--- Week5 - KD-Trees/src/PointSET.java
+++ Week5 - KD-Trees/src/PointSET.java
@@ -13,60 +13,78 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class PointSET {
+public class PointSET 
+{
 
     private SET<Point2D> points = new SET<>();
 
-
-    public boolean isEmpty() {   // is the set empty?
+    // is the set empty?
+    public boolean isEmpty() 
+    {   
         return points.isEmpty();
     }
 
-    public int size() {   // number of points in the set
+    // number of points in the set
+    public int size() 
+    {   
         return points.size();
     }
 
-    public void insert(Point2D p) {   // add the point to the set (if it is not already in the set)
+    // add the point to the set (if it is not already in the set)
+    public void insert(Point2D p) 
+    {   
         checkNotNull(p, "Not supported to insert null as point");
         points.add(p);
     }
 
-    public boolean contains(Point2D p) {   // does the set contain point p?
+    // does the set contain point p?
+    public boolean contains(Point2D p) 
+    {   
         checkNotNull(p, "Null is never contained in a PointSET");
         return points.contains(p);
     }
 
-    public void draw() {   // draw all points to standard draw
+    // draw all points to standard draw
+    public void draw() 
+    {   
         for (Point2D point : points) {
             StdDraw.point(point.x(), point.y());
         }
     }
 
-    public Iterable<Point2D> range(RectHV rect) {   // all points that are inside the rectangle
+    // all points that are inside the rectangle
+    public Iterable<Point2D> range(RectHV rect) 
+    {   
         checkNotNull(rect, "Can't calculate range for a rect will value null");
 
         List<Point2D> solution = new ArrayList<>();
-        for (Point2D point : points) {
-            if (rect.contains(point)) {
+        for (Point2D point : points) 
+        {
+            if (rect.contains(point)) 
+            {
                 solution.add(point);
             }
         }
         return solution;
     }
 
-    public Point2D nearest(Point2D p) {   // a nearest neighbor in the set to point p; null if the set is empty
+    // a nearest neighbor in the set to point p; null if the set is empty
+    public Point2D nearest(Point2D p) 
+    {   
         checkNotNull(p, "Can't calculate nearest point to a point with value null");
 
         Point2D nearestPoint = null;
         for (Point2D point : points) {
-            if (nearestPoint == null || point.distanceTo(p) < nearestPoint.distanceTo(p)) {
+            if (nearestPoint == null || point.distanceTo(p) < nearestPoint.distanceTo(p)) 
+            {
                 nearestPoint = point;
             }
         }
         return nearestPoint;
     }
 
-    private static void checkNotNull(Object o, String messageIfObjectIsNull) {
+    private static void checkNotNull(Object o, String messageIfObjectIsNull) 
+    {
         if (o == null) throw new NullPointerException(messageIfObjectIsNull);
     }
 
